2a084f22e25fbfbb14f976dbc9d5a3c72cbe3181
Gregg <chopstyix@gmail.com>
2021-01-03 13:56:45 -0800

    bug fixes

--- autoupdate.lua
+++ autoupdate.lua
@@ -10,6 +10,7 @@ if turtle then
     print("I'm a turtle!")
     for _,filename in fs.list("") do
         if not ignore[filename] then
+            fs.delete(filename)
             fs.copy("downloads/cEEL/cTurtle/"..filename,filename) 
         end
     end
