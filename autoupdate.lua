de9274369f99e7948b0d4315b1b6ca2de02dbc2b
Gregg <chopstyix@gmail.com>
2021-01-03 15:22:52 -0800

    changed github download api

--- autoupdate.lua
+++ autoupdate.lua
@@ -3,13 +3,14 @@ local ignore = {
     rom = true
 }
 
-shell.run("github","chopstyix","cEEL")
-shell.run("clear")
+-- shell.run("github","chopstyix","cEEL")
+-- shell.run("clear")
+shell.run("github","chopstyix/cEEL","-a chopstyix")
 print("Detecting source...")
 os.sleep(0.5)
 if turtle then
     print("I'm a turtle!")
-    for _,filename in ipairs(fs.list("")) do
+    for _,filename in ipairs(fs.list("downloads/cEEL/cTurtle/")) do
             fs.delete(filename)
             fs.copy("downloads/cEEL/cTurtle/"..filename,filename) 
         end
