c10985fb9cb1819442858df6767f838186a4ae01
Gregg <chopstyix@gmail.com>
2021-01-19 17:34:35 -0800

    bug fix

--- cAsino/farkle/startup.lua
+++ cAsino/farkle/startup.lua
@@ -10,6 +10,39 @@ SLOT_4 = {1,18}
 SLOT_5 = {18,18}
 SLOT_6 = {35,18}
 
+Player = {
+    flag_skip = false,
+    flag_bust = false,
+    input = nil,
+    bet = 0,
+    total_score = 0,
+    turn_score = 0,
+    hand = {    
+      [1] = {value = nil, hold = false, lock = false, flag_count = false},
+      [2] = {value = nil, hold = false, lock = false, flag_count = false},
+      [3] = {value = nil, hold = false, lock = false, flag_count = false},
+      [4] = {value = nil, hold = false, lock = false, flag_count = false},
+      [5] = {value = nil, hold = false, lock = false, flag_count = false},
+      [6] = {value = nil, hold = false, lock = false, flag_count = false},
+    },
+    hold = {},
+  }
+
+function Player:rollDice()
+-- local x = 1
+local rolls = {math.random(6),math.random(6),math.random(6),math.random(6),math.random(6),math.random(6)}
+-- print("Showing rolls")
+-- print(rolls[1],rolls[2],rolls[3],rolls[4],rolls[5],rolls[6])
+-- os.sleep(2)
+for k,v in ipairs(self.hand) do
+    -- print(k,v)
+    if self.hand[k].lock == false then -- If dice is not locked then roll a new value.
+    self.hand[k].value = rolls[k]
+    -- x = x + 1
+    end
+end
+-- x = 0
+end
 function setup()
     surface = dofile("cEEL/cAsino/farkle/surface")
     diceMon = peripheral.wrap("monitor_14")
@@ -29,7 +62,7 @@ function setup()
     diceBg = surface.load("cEEL/cAsino/farkle/diceBg.nfp")
 end
 
-function drawDice(value)
+function Player:drawDice(value)
     -- local value_buffer = value
     local dice = surface.create(17,17)
     local number = surface.load("cEEL/cAsino/farkle/"..value..".nfp")
@@ -69,9 +102,12 @@ end
 
 -- Main Line Code -- 
 -- Dice img dimensions are 13 x 9
+p1 = Player -- User
+p2 = Player -- Computer Opponent
 setup()
+
 screen:clear(colors.green)
-drawPlayerHand("1","2","3","4","5","6")
+p1:drawPlayerHand(self.hand[1],self.hand[2],self.hand[3],self.hand[4],self.hand[5],self.hand[6])
 screen:output()
 --term.redirect(oldTerm)
 --print("test1")
