945a11a20fbfb3fe275c61ecc24871fd736c68d9
Gregg <chopstyix@gmail.com>
2021-01-21 01:06:14 -0800

    bug fix

--- cAsino/farkle/startup.lua
+++ cAsino/farkle/startup.lua
@@ -90,6 +90,8 @@ end
 function Player:checkState(state)
     local valid = false
     local logtable = {}
+    local count = 0
+    local score = 0
     -- I don't understand how this works, but it count's the number of times each diceValue is repeated.
     if state == "hold" then
         debug("Checking hold")
@@ -110,33 +112,165 @@ function Player:checkState(state)
             end
         end
     end
+    count = countTable(logtable)
 
-    local count = countTable(logtable)
-    if (count == 6) then
-        debug("Detected a 6 dice straight")
-        valid = true
-    elseif (count == 5) then
-        if (logtable[1] == 1) and (logtable[5] == 5) then
-        debug("Detected a 5 dice straight (1 to 5)")
-        valid = true
-        elseif (logtable[1] == 2) and (logtable[5] == 6) then
-        debug("Detected a 5 dice straight (2 to 6)")
-        valid = true
-        end
-    else
-        for diceValue,match in pairs(logtable) do -- Check for valid match, return a
-        debug("diceValue: "..diceValue.."  match: "..match)
-        if (match >= 3) then
-            debug("Detected a minimum of 3 of a kind")
+    -- for i,v in pairs (self.hand) do
+        -- if self.hand[i].flag_count == false then
+    repeat
+        if (count == 6) and  then
+            debug("Detected a 6 dice straight")
             valid = true
-        elseif (diceValue == 1 or diceValue == 5) then
-            debug("Detected a 1 or a 5")
-            valid = true
+            score = score + 1500
+            self.hand[1].flag_count == true
+            self.hand[2].flag_count == true
+            self.hand[3].flag_count == true
+            self.hand[4].flag_count == true
+            self.hand[5].flag_count == true
+            self.hand[6].flag_count == true
+            count = 0
+        elseif (count == 5) then
+            if (logtable[1] == 1) and (logtable[5] == 5) then
+                debug("Detected a 5 dice straight (1 to 5)")
+                valid = true
+                score = score + 500
+                for i,v in pairs(self.hand) do
+                    if v.value == 1 then
+                        self.hand[i].flag_count = true
+                    elseif v.value == 2 then
+                        self.hand[i].flag_count = true
+                    elseif v.value == 3 then
+                        self.hand[i].flag_count = true
+                    elseif v.value == 4 then
+                        self.hand[i].flag_count = true
+                    elseif v.value == 5 then
+                        self.hand[i].flag_count = true
+                    end
+                end
+                count = 0
+                flag_counter = 5
+            elseif (logtable[1] == 2) and (logtable[5] == 6) then
+                debug("Detected a 5 dice straight (2 to 6)")
+                valid = true
+                score = score + 750
+                if v.value == 2 then
+                    self.hand[i].flag_count = true
+                elseif v.value == 3 then
+                    self.hand[i].flag_count = true
+                elseif v.value == 4 then
+                    self.hand[i].flag_count = true
+                elseif v.value == 5 then
+                    self.hand[i].flag_count = true
+                elseif v.value == 6 then
+                    self.hand[i].flag_count = true
+                end
+                count = 0
+                flag_counter = 5
+            end
+        else
+            for diceValue,match in pairs(logtable) do 
+                debug("diceValue: "..diceValue.."  match: "..match)
+                if (match == 6) then
+                    debug("Detected a 6 of a kind")
+                    valid = true
+                    if diceValue == 1 then
+                        score = score + 8000
+                    elseif diceValue == 2 then
+                        score = score + 1600
+                    elseif diceValue == 3 then
+                        score = score + 2400
+                    elseif diceValue == 4 then
+                        score = score + 3200
+                    elseif diceValue == 5 then
+                        score = score + 4000
+                    elseif diceValue == 6 then
+                        score = score + 4800
+                    end
+                    self.hand[1].flag_count == true
+                    self.hand[2].flag_count == true
+                    self.hand[3].flag_count == true
+                    self.hand[4].flag_count == true
+                    self.hand[5].flag_count == true
+                    self.hand[6].flag_count == true
+                elseif (match == 5) then
+                    debug("Detected a 5 of a kind")
+                    valid = true
+                    if diceValue == 1 then
+                        score = score + 4000
+                    elseif diceValue == 2 then
+                        score = score + 800
+                    elseif diceValue == 3 then
+                        score = score + 1200
+                    elseif diceValue == 4 then
+                        score = score + 1600
+                    elseif diceValue == 5 then
+                        score = score + 2000
+                    elseif diceValue == 6 then
+                        score = score + 2400
+                    end
+                    for i,v in pairs(self.hand) do
+                        if self.hand[i].value == diceValue then
+                            self.hand[i].flag_count == true
+                        end
+                    end
+                elseif (match == 4) then
+                    debug("Detected a 4 of a kind")
+                    valid = true
+                    if diceValue == 1 then
+                        score = score + 2000
+                    elseif diceValue == 2 then
+                        score = score + 400
+                    elseif diceValue == 3 then
+                        score = score + 600
+                    elseif diceValue == 4 then
+                        score = score + 800
+                    elseif diceValue == 5 then
+                        score = score + 1000
+                    elseif diceValue == 6 then
+                        score = score + 1200
+                    end
+                    for i,v in pairs(self.hand) do
+                        if self.hand[i].value == diceValue then
+                            self.hand[i].flag_count == true
+                        end
+                    end
+                elseif (match == 3) then
+                    debug("Detected a 3 of a kind")
+                    valid = true
+                    if diceValue == 1 then
+                        score = score + 1000
+                    elseif diceValue == 2 then
+                        score = score + 200
+                    elseif diceValue == 3 then
+                        score = score + 300
+                    elseif diceValue == 4 then
+                        score = score + 400
+                    elseif diceValue == 5 then
+                        score = score + 500
+                    elseif diceValue == 6 then
+                        score = score + 600
+                    end
+                    for i,v in pairs(self.hand) do
+                        if self.hand[i].value == diceValue then
+                            self.hand[i].flag_count == true
+                        end
+                    end
+                elseif (diceValue == 1) and self.hand[i].flag_count == false then
+                    if
+                    debug("Detected a 1")
+                    valid = true
+                    score = score + 100
+                    self.hand[i].flag_count == true
+                elseif (diceValue == 5) and self.hand[i].flag_count == false then
+                    debug("Detected a 5")
+                    valid = true
+                    score = score + 50
+                    self.hand[i].flag_count == true
+                end
+            end
         end
-        end
-    end
-    -- local key = os.pullEvent("key")
-return valid
+    print (card_counter())
+    until (card_counter() == 6)
+return valid, score
 end
 
 function countTable(table)
@@ -158,11 +292,22 @@ function Player:drawScreen()
     screen:drawSurface(drawButton("ROLL",colors.white),58,2)
     screen:drawSurface(drawButton("SKIP",colors.white),58,12)
     screen:drawSurface(drawButton("QUIT",colors.red),58,30)
-    screen:drawSurface(drawButton(tostring(self.total_score),colors.white),0,30)
+    screen:drawSurface(drawButton(tostring(self.turn_score),colors.white),0,31)
+    screen:drawSurface(drawButton(tostring(self.total_score),colors.white),30,31)
     if self.flag_bust then
         screen:drawSurface(drawButton("BUST",colors.red),25,15)
     end
     screen:output()
+end
+
+function Player:card_counter()
+    local value = 0
+    for i,v in pairs(self.hand) do
+        if self.hand[i].flag_count == true then
+            value = value + 1
+        end
+    end
+    return value
 end
 
 function Player:holdDice_phase()
@@ -194,6 +339,7 @@ function Player:holdDice_phase()
                     elseif self.hand[i].hold == true and self.hand[i].lock == false then
                         self.hand[i].hold = false
                     end
+                    -- Calculate potential value everytime a dice is toggled with
                 elseif i == 7 then -- Roll
                     if self:checkState("hold") then
                         for i,v in pairs(self.hand) do
@@ -203,6 +349,8 @@ function Player:holdDice_phase()
                             end
                         end
                         loop = false
+                        -- Pass turn_score to total score
+                        
                     end
                 elseif i == 8 then -- 
                     loop = false
@@ -213,6 +361,7 @@ function Player:holdDice_phase()
                 end
             end
         end
+        self.turn_score = 0
         screen:output()
     end
 end
